3bae4f295ecfff21f1f3e44e27805c36b70d05c4
emirhanaricii <emirhan190341@gmail.com>
2023-03-17 17:16:48 +0300

    Added new changes

--- inventory-service/src/main/java/com/emirhanarici/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/emirhanarici/inventoryservice/repository/InventoryRepository.java
@@ -2,10 +2,8 @@ package com.emirhanarici.inventoryservice.repository;
 
 import com.emirhanarici.inventoryservice.entity.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
--- product-service/src/main/java/com/emirhanarici/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/emirhanarici/productservice/ProductServiceApplication.java
@@ -3,10 +3,12 @@ package com.emirhanarici.productservice;
 import com.emirhanarici.productservice.repository.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories(basePackageClasses = ProductRepository.class)
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
